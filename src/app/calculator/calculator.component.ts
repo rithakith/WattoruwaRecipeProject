f926529c452c92da6438e478185c83d69ff90010
rithakith <ritharaedirisinghe@gmail.com>
2023-08-26 08:32:10 +0530

    calculator done

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,45 +1,67 @@
 import { Component } from '@angular/core';
 
+interface User {
+  name: string;
+  age: number;
+  gender: 'male' | 'female';
+  formula: 'mifflin' | 'revised' | 'katch';
+  height:number;
+  weight:number;
+  fat:number;
+}
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
-  age: number = 0;
-  gender: number = 0;
-  bodyFat: number = 0;
-  height: number = 0;
-  weight: number = 0;
-  activity: number = 0;
-  unit: number = 1;
-  formula: number = 0;
-  calorieResult: string = '';
+  user: User = {
+    name: '',
+    age: 0,
+    gender: 'male',
+    formula: 'mifflin',
+    height:0,
+    weight:0,
+    fat:0,
+  };
 
-  calculateCalories() {
-    let bmr: number=0;
+  showResults: boolean = false;
+  maintainCalories: number = 0;
+  mildLossCalories: number = 0;
+  weightLossCalories: number = 0;
+  extremeLossCalories: number = 0;
 
-    if (this.formula === 0) {
-      if (this.gender === 0) {
-        bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
-      } else {
-        bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
-      }
-    } else if (this.formula === 1) {
-      if (this.gender === 0) {
-        bmr = 13.397 * this.weight + 4.799 * this.height - 5.677 * this.age + 88.362;
-      } else {
-        bmr = 9.247 * this.weight + 3.098 * this.height - 4.33 * this.age + 447.593;
-      }
-    } else if (this.formula === 2) {
-      bmr = 370 + 21.6 * (1 - this.bodyFat / 100) * this.weight;
-    }
+  submitForm() {
+    const name = this.user.name;
+    const weightInKg = this.user.weight;
+    const heightInCm = this.user.height;
+    const age = this.user.age;
+    const fat = this.user.fat;
+    const percent_fat = fat / 100;
+  
+    // Calculate BMR values based on formula, gender, and goal
+    const mifflinBMR = (this.user.gender === 'male')
+      ? (10 * weightInKg) + (6.25 * heightInCm) - (5 * age) + 5
+      : (10 * weightInKg) + (6.25 * heightInCm) - (5 * age) - 161;
+  
+    const revisedBMR = (this.user.gender === 'male')
+      ? (13.397 * weightInKg) + (4.799 * heightInCm) - (5.677 * age) + 88.362
+      : (9.247 * weightInKg) + (3.098 * heightInCm) - (4.33 * age) + 447.593;
+  
+    const katchBMR = 370 + 21.6 * (1 - percent_fat) * weightInKg;
+  
+    // Calculate caloric intake values for each goal
+    const maintainCalories = mifflinBMR;
+    const mildLossCalories = mifflinBMR * 0.9;
+    const weightLossCalories = mifflinBMR * 0.79;
+    const extremeLossCalories = mifflinBMR * 0.59;
+  
+    // Display calculated values
+    this.maintainCalories = Math.floor(maintainCalories);
+    this.mildLossCalories = Math.floor(mildLossCalories);
+    this.weightLossCalories = Math.floor(weightLossCalories);
+    this.extremeLossCalories = Math.floor(extremeLossCalories);
 
-    let ret: number = bmr * this.activity;
-    if (this.unit === 11) {
-      ret = ret * 4.1868;
-    }
-
-    this.calorieResult = `Calorie Amount: ${ret.toFixed(2)} Calories`;
+    this.showResults = true; // Display the results div
   }
 }
