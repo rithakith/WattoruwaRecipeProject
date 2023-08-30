d2a7698105b1e54621a5632c721e5ba68667b8b8
rithakith <ritharaedirisinghe@gmail.com>
2023-08-30 18:47:03 +0530

    cookTime for menu

--- src/app/menu/menu.model.ts
+++ src/app/menu/menu.model.ts
@@ -2,10 +2,14 @@ export class Menu_items{
     public name:string;
     public description:string;
     public imagePath:string;
+    public stars:number;
+    public cookTime:string;
 
-    constructor(name:string, desc:string, imagePath:string){
+    constructor(name:string, desc:string, imagePath:string,stars:number,cookTime:string){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.stars = stars;
+        this.cookTime=cookTime;
     }
 }
